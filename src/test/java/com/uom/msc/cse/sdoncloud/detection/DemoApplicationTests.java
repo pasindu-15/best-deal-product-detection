a047e9a023a9e06cba91af652bfe8b49f8b3d941
Pasindu <pasindu.hewagamage@axiatadigitallabs.com>
2021-01-31 20:29:14 +0530

    initial commit

---
+++ src/test/java/com/uom/msc/cse/sdoncloud/detection/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uom.msc.cse.sdoncloud.detection;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DemoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
