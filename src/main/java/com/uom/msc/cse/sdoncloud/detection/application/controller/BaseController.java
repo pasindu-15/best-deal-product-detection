f2cd9fd1412a32d14113d3eb8f4b9eba892351f3
Pasindu <hewagamagepasindu@gmail.com>
2021-03-03 23:31:56 +0530

    re added APis

--- src/main/java/com/uom/msc/cse/sdoncloud/detection/application/controller/BaseController.java
+++ src/main/java/com/uom/msc/cse/sdoncloud/detection/application/controller/BaseController.java
@@ -1,24 +1,24 @@
-//
-//package com.uom.msc.cse.sdoncloud.detection.application.controller;
-//
-//import com.uom.msc.cse.sdoncloud.detection.application.config.YAMLConfig;
-//import org.slf4j.MDC;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import java.util.UUID;
-//
-//public class BaseController {
-//    @Autowired
-//    private YAMLConfig yamlConfig;
-//
-//    public void setLogIdentifier(HttpServletRequest request) {
-//
-//        String logIdentifier = request.getHeader(yamlConfig.getLogIdentifierKey());
-//        if (logIdentifier != null) {
-//            MDC.put(yamlConfig.getLogIdentifierKey(), logIdentifier);
-//        } else {
-//            MDC.put(yamlConfig.getLogIdentifierKey(), UUID.randomUUID().toString());
-//        }
-//    }
-//}
+
+package com.uom.msc.cse.sdoncloud.detection.application.controller;
+
+import com.uom.msc.cse.sdoncloud.detection.application.config.YAMLConfig;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
+public class BaseController {
+    @Autowired
+    private YAMLConfig yamlConfig;
+
+    public void setLogIdentifier(HttpServletRequest request) {
+
+        String logIdentifier = request.getHeader(yamlConfig.getLogIdentifierKey());
+        if (logIdentifier != null) {
+            MDC.put(yamlConfig.getLogIdentifierKey(), logIdentifier);
+        } else {
+            MDC.put(yamlConfig.getLogIdentifierKey(), UUID.randomUUID().toString());
+        }
+    }
+}
